10dd79c49a4c3f1ef31d6208e655e17b42d74348
ethanandrews <eandrews@whoi.edu>
2023-12-11 09:46:10 -0500

    update image names

--- docker_image_build.sh
+++ docker_image_build.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker buildx build -f compose/production/django/Dockerfile -t ghcr.io/whoigit/ooicgsn-roundabout:1.7.0 --platform linux/amd64 --push .
+docker buildx build -f compose/production/django/Dockerfile -t ghcr.io/whoigit/obs-roundabout:1.7.0 --platform linux/amd64 --push .
